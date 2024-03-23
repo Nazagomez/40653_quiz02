f8d7a98a79bf90b17d2640d3fa55cb3cfa930c8f
Naza Gomez <nazasanchez12.26@gmail.com>
2024-03-23 15:19:43 -0600

    quiz 2

--- src/context/MyContext.tsx
+++ src/context/MyContext.tsx
@@ -1,39 +1,18 @@
+import React from "react";
 
-import React, { createContext, useContext, useState } from 'react';
-
-const initialState = {
-  products: [
-    { id:1, step:1, name: 'Mouse', price: 60 },
-    { id:2, step:1, name: 'Monitor', price: 70 },
-    { id:4, step:2, name: 'Keyboard', price: 80 },
-    { id:5, step:2, name: 'Headset', price: 90 },
-    { id:6, step:3, name: 'Tablet', price: 100 },
-    { id:7, step:3, name: 'Hub', price: 110 }
-  ],
-  currentStep: 1, 
-
-  
-  
-};
-
-export const MyContext = createContext(initialState);
-
-export const useMyContext = () => useContext(MyContext);
-
-export const MyProvider = ({ children }) => {
-  const [state, setState] = useState(initialState);
-
-  type Product = {
+type Product = {
     id: number;
     name: string;
     step: number;
     price: number;
 }
 
+type MyContextData = {
+    
+}
 
-  return (
-    <MyContext.Provider value={state}>
-      {children}
-    </MyContext.Provider>
-  );
-};
+const MyContext = React.createContext<MyContextData>({
+
+});
+
+export default MyContext;
